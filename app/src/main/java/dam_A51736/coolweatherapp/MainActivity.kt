37697c9f13e48e412376e236ed3223b4700ff79c
marti <martim.ramos2005@gmail.com>
2026-04-04 19:06:58 +0100

    Início do desenvolvimento da aplicação "Cool Weather ‘App’". É uma aplicação Android (.apk) realizada no ambiente Android Studio. Fase da UI em desenvolvimento...

---
+++ app/src/main/java/dam_A51736/coolweatherapp/MainActivity.kt
@@ -0,0 +1,41 @@
+package dam_A51736.coolweatherapp
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    // Define se é dia ou noite manualmente
+    var day = false
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        // A escolha do tema tem de acontecer ANTES das instruções super.onCreate e setContentView
+        when (resources.configuration.orientation) {
+            android.content.res.Configuration.ORIENTATION_PORTRAIT -> {
+                if (day) {
+                    setTheme(R.style.Theme_Day)
+                } else {
+                    setTheme(R.style.Theme_Night)
+                }
+            }
+            android.content.res.Configuration.ORIENTATION_LANDSCAPE -> {
+                if (day) {
+                    setTheme(R.style.Theme_Day_Land)
+                } else {
+                    setTheme(R.style.Theme_Night_Land)
+                }
+            }
+        }
+
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.container)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
